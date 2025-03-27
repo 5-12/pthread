2d77bb4c5594aa0822a8b064965de88ae4b3c08b
5-12 <hellowordare@163.com>
2025-03-27 14:13:59 +0800

    生产通知消费<未完>

--- main.cpp
+++ main.cpp
@@ -3,20 +3,32 @@
 #include <mutex>
 #include <vector>
 #include <atomic>
-std::vector<int> test[1000];
+#include <queue>
+std::queue<int> test[1000];
 std::mutex mu;
-std::atomic<int>test[100];
+//std::atomic<int>test[100];
+std::atomic<bool>max{ false };
+std::atomic<bool>nome{ false };
+std::condition_variable cv;
 void in(void)
 {
-	std::unique_lock<std::mutex> mu;
-
+	while (true)
+	{
+		std::unique_lock<std::mutex> ;
+	}
 }
 void out(void)
 {
-	std::unique_lock<std::mutex> mu;
-
+	while (true)
+	{
+		
+	}
+	
 }
 int main(void)
 {
-	;
+	std::thread in_pr(in);
+	std::thread out_pr(out);
+	in_pr.join();
+	out_pr.join();
 }
